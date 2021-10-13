11ee62d9a7e4ccfd6eb7759591574c3e959cc63e
Asdrubalini <indirizzoemail@mail.com>
2021-10-14 01:27:38 +0200

    don't panic

--- src/telegram/mod.rs
+++ src/telegram/mod.rs
@@ -72,8 +72,8 @@ pub async fn handle_messages(api: &Api) {
         let update = match update {
             Ok(update) => update,
             Err(error) => {
-                panic!("Event error: {:?}", error);
-                // continue;
+                println!("Event error: {:?}", error);
+                continue;
             }
         };
 
