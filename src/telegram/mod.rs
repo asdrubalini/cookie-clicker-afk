22493e1b0a6118cb034316b40287fe8628d8859f
Asdrubalini <indirizzoemail@mail.com>
2021-10-13 21:02:31 +0200

    disallow users other from admin

--- src/telegram/mod.rs
+++ src/telegram/mod.rs
@@ -1,7 +1,11 @@
-use std::sync::Arc;
+use std::{
+    convert::{TryFrom, TryInto},
+    env,
+    sync::Arc,
+};
 
 use futures::StreamExt;
-use telegram_bot::{Api, ChatId, MessageKind, SendMessage};
+use telegram_bot::{Api, ChatId, Message, MessageKind, SendMessage, UserId};
 use tokio::sync::Mutex;
 
 use crate::cookie_clicker::CookieClicker;
@@ -49,6 +53,18 @@ async fn command_task(api: Api, command_data: CommandData, chat_id: ChatId) {
     });
 }
 
+/// Validates user identity from message
+fn is_user_admin(message: &Message) -> bool {
+    let admin_id = UserId::from(
+        env::var("TELEGRAM_ADMIN_ID")
+            .expect("Missing env TELEGRAM_ADMIN_ID")
+            .parse::<i64>()
+            .expect("Invalid TELEGRAM_ADMIN_ID"),
+    );
+
+    message.from.id == admin_id
+}
+
 pub async fn handle_messages(api: &Api) {
     let cookie_clicker: ConcurrentCookieClicker = Arc::new(Mutex::new(
         CookieClicker::new()
@@ -70,6 +86,11 @@ pub async fn handle_messages(api: &Api) {
         };
 
         if let telegram_bot::UpdateKind::Message(message) = update.kind {
+            if !is_user_admin(&message) {
+                println!("Some user tried to access the bot");
+                continue;
+            }
+
             if let MessageKind::Text { data, .. } = message.kind {
                 let api = api.clone();
                 let chat_id = message.chat.id();
